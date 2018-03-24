d59acf026491f5dd8599c5fc278b3db255776713
Jan Gloser <jan.renra.gloser@gmail.com>
2018-03-24 23:28:40 +0100

    Cycle

---
+++ cycle.rb
@@ -0,0 +1,19 @@
+prd = "Nesmrdi tu nejaky prezident?"
+puts prd
+
+odpoved = gets
+odpoved = odpoved.chomp
+
+
+puts odpoved.class
+
+if odpoved == "Ano"
+  puts "Pak je to Pepik"  
+else
+ if odpoved == "Ne"
+ puts "Pak smrdi Miluska"
+ end
+end
+
+
+a,b,c = integer
